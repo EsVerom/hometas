e66a4f080c61732aba66f68d2d721610f06f34ea
EsVerom <morevodnako@gmail.com>
2022-05-11 19:05:40 +0300

    TrinagleTask done+update

--- src/main/java/com/javalesson/chapter1/task1/TriangleTask.java
+++ src/main/java/com/javalesson/chapter1/task1/TriangleTask.java
@@ -14,30 +14,25 @@ public class TriangleTask {
     public static void main(String[] args) {
         System.out.println("Please enter three sides of a triangle");
         Scanner in = new Scanner(System.in);
-// Enter sides
         int a=in.nextInt();
         int b=in.nextInt();
         int c=in.nextInt();
 
-        //Check the biggest side
-
-        int biggest = 0;
-        int sum = 0 ;
+        int biggest;
+        int sum;
         if (a > b) {
             biggest = a;
             sum = c + b;
         }
-        else if (b>c){
+        else if (b > c){
             biggest = b;
             sum = c + a;
         }
-        else if (c > a) {
+        else {
             biggest = c;
             sum = a + b;
         }
-//        System.out.println("The longest side is " + biggest);
-
-        //Check that summ of other sides less than biggest side
+        System.out.println("biggest = " + biggest + ", sum = " + sum);
 
         if (a < 1 || b < 1 || c < 1){
             System.out.println("Side cannot be less 1");
