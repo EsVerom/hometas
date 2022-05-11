87452a82b8dfc759c3497e83f21def999b18fbc2
EsVerom <morevodnako@gmail.com>
2022-05-11 17:23:19 +0400

    add exclamation mark

--- src/main/java/com/javalesson/chapter1/task0/LoopSolution.java
+++ src/main/java/com/javalesson/chapter1/task0/LoopSolution.java
@@ -8,7 +8,7 @@ public class LoopSolution {
         Scanner in = new Scanner(System.in);
         int t=in.nextInt();
         for(int i=0;i<t;i++){
-        System.out.println("Please enter parameters");
+        System.out.println("Please enter parameters!");
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
