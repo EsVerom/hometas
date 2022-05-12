1b1f47a4773d47318de6dda74f94fcfb6cd12f3e
EsVerom <morevodnako@gmail.com>
2022-05-12 17:38:05 +0400

    add exclamation mark

--- src/main/java/com/javalesson/chapter1/task3/PalindromeFinderTask.java
+++ src/main/java/com/javalesson/chapter1/task3/PalindromeFinderTask.java
@@ -1,6 +1,8 @@
 package com.javalesson.chapter1.task3;
 
 
+import java.util.Scanner;
+
 /**
  * Напишите программу которая  проверяет введенное с клавиатуры число и говорит
  * является ли данное число палиндромом.
@@ -19,6 +21,33 @@ package com.javalesson.chapter1.task3;
 public class PalindromeFinderTask {
 
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a five-digit number");
+        int instertNum=in.nextInt();
+        while ( instertNum < 10000 || instertNum > 99999){
+            System.out.println("Please enter again a five-digit number");
+            instertNum=in.nextInt();
+        }
+        System.out.println("Your number = " + instertNum);
+        int lastNum = instertNum % 10;
+        int firstNum = instertNum / 10000;
+        System.out.println("First digit = " + firstNum + "; Last digit = " + lastNum );
+
+        if (lastNum == firstNum) {
+            int secondLastNum = (instertNum % 100) / 10;
+            int secondNum = (instertNum / 1000) % 10;
+            System.out.println("Second digit = " + secondNum + "; Pre Last digit = " + secondLastNum);
+            if (secondNum == secondLastNum) {
+                System.out.println("Congratulations, your number is palindrome!");
+            }
+            else {
+                System.out.println("Sorry but your number is not palindrome");
+            }
+        }
+        else {
+            System.out.println("Sorry but your number is not palindrome");
+        }
 
     }
 }
